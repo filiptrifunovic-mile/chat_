e8627ac4418ad72ed6fe3c087a10766aa0c25f83
unknown <filip.trifunovic23@gmail.com>
2023-02-19 18:02:40 +0100

    jwt token, creating schema

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URL);
+const User = require("./models/User");
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET;
 
 const app = express();
 
@@ -9,6 +12,13 @@ app.get("/test", (req, res) => {
   res.json("test ok");
 });
 
-app.post("/register", (req, res) => {});
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const createdUser = await User.create({ username, password });
+  jwt.sign({ userId: createdUser._id }, jwtSecret, (err, token) => {
+    if (err) throw err;
+    res.cookie("token", token).status(201).json("ok");
+  });
+});
 
 app.listen(4000);
