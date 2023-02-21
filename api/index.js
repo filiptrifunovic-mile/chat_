be1e3d35e6f0a28f35e958b75ffc861fdaccd3a9
unknown <filip.trifunovic23@gmail.com>
2023-02-21 14:59:22 +0100

    getting cookie from registered user after refresh

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const User = require("./models/User");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL, (err) => {
@@ -13,6 +14,7 @@ const jwtSecret = process.env.JWT_SECRET;
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
@@ -29,15 +31,36 @@ app.post("/register", async (req, res) => {
 
   try {
     const createdUser = await User.create({ username, password });
-    jwt.sign({ userId: createdUser._id }, jwtSecret, {}, (err, token) => {
-      if (err) throw err;
-      res.cookie("token", token).status(201).json({
-        id: createdUser._id,
-      });
-    });
+    jwt.sign(
+      { userId: createdUser._id, username },
+      jwtSecret,
+      {},
+      (err, token) => {
+        if (err) throw err;
+        res
+          .cookie("token", token, { sameSite: "none", secure: true })
+          .status(201)
+          .json({
+            id: createdUser._id,
+          });
+      }
+    );
   } catch (error) {
     if (error) throw error;
     res.status(500).json("ok");
+  }
+});
+
+app.get("/profile", (req, res) => {
+  const token = req.cookies?.token;
+
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, (err, userData) => {
+      if (err) throw err;
+      res.json(userData);
+    });
+  } else {
+    res.status(401).json("no token, sry");
   }
 });
 
--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -1,10 +1,17 @@
-import { createContext, useState } from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
   const [username, setLoggedInUsername] = useState(null);
   const [id, setId] = useState(null);
+
+  useEffect(() => {
+    axios.get("/profile").then((res) => {
+      console.log(res.data);
+    });
+  }, []);
 
   return (
     <UserContext.Provider
